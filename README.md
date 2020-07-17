a853ca26ac861d6394932bfbc734dd433a67120c
kn-km <974787396@qq.com>
2020-07-17 14:15:04 +0800

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# Random Teleport
+
+****Introduction****
+This is a high performance path for CMI Random Teleport.
+
+****Notice****
+This is not a standalone plugin.
+You should install [CMI](https://www.spigotmc.org/resources/cmi-270-commands-insane-kits-portals-essentials-economy-mysql-sqlite-much-more.3742/updates) first.
---
+++ src/main/java/me/knkm/randomteleport/RandomTeleport.java
@@ -0,0 +1,75 @@
+package me.knkm.randomteleport;
+
+import com.Zrips.CMI.Modules.tp.Teleportations;
+import com.Zrips.CMI.events.CMIPlayerTeleportEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.Chunk;
+import org.bukkit.Location;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+public final class RandomTeleport extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onCMIPlayerTeleport(CMIPlayerTeleportEvent event) {
+        if (event.getType() == Teleportations.TeleportType.randomTp && event.getSafe().getTpCondition() == Teleportations.TpCondition.Good) {
+            Location to = event.getTo();
+            event.setCancelled(true);
+            if (event.isAsynchronous()) {
+                loadNearby(to);
+                event.getPlayer().teleport(to);
+            } else {
+                Bukkit.getScheduler().runTaskAsynchronously(this, () ->{
+                    loadNearby(to);
+                    Bukkit.getScheduler().runTask(this, () -> {
+                        event.getPlayer().teleportAsync(to);
+                    });
+                });
+            }
+        }
+
+    }
+
+    private void loadNearby(Location location) {
+        List<CompletableFuture<Chunk>> asyncChunk = new ArrayList<>();
+        for (int i = -4; i < 5; i++) {
+            int delx = i * 16;
+            for (int j = -4; j < 5; j++) {
+                int delz = j * 16;
+                Location l1 = new Location(location.getWorld(), location.getX() + delx, location.getY(), location.getZ() + delz);
+                CompletableFuture<Chunk> chunk = l1.getWorld().getChunkAtAsync(l1,true);
+                asyncChunk.add(chunk);
+            }
+        }
+        while (!check(asyncChunk)) {
+            try {
+                //wait for async chunk load
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    private boolean check(List<CompletableFuture<Chunk>> asyncChunk) {
+        boolean flag = true;
+        for (CompletableFuture<Chunk> chunk : asyncChunk) {
+            if (!chunk.isDone()) {
+                flag = false;
+            }
+        }
+        System.out.println(flag);
+        return flag;
+    }
+}
